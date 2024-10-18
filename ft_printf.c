84c027b146e4100f16cab555a8b1139fcd5acef7
Noa Charbogne <noa.charbogne@orange.fr>
2024-10-18 15:16:52 +0200

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,38 +6,72 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 10:02:38 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/18 11:42:13 by ncharbog         ###   ########.fr       */
+/*   Updated: 2024/10/18 15:13:29 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+void	ft_format_char(int c)
+{
+	char	a;
+	
+	a = (char)c;
+	if (a)
+		write(1, &a, 1);
+}
+
+void	ft_format_str(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
 
 int	ft_printf(char const *format, ...)
 {
 	int		i;
 	int		c;
+	char 	*s;
 	va_list	args;
-	char	*dest;
 
 	i = 0;
-	args = 0;
 	c = 0;
+	s = NULL;
 	va_start(args, format);
 	while (format[i])
 	{
 		if (format[i] == '%')
+		{
 			i++;
 			if (format[i] == '%')
-			{
-				dest = ft_strdup("%");
-				ft_putstr(dest);
-			}
+				write(1, &format[i], 1);
 			if (format[i] == 'c')
-				int c = va_arg(args, int);
-				ft_format_char(format[i], c);
+			{
+				c = va_arg(args, int);
+				ft_format_char(c);
+			}
+			if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				ft_format_str(s);
+			}
 			i++;
-		ft_putchar(&format[i]);
+		}
+		write(1, &format[i], 1);
 		i++;
 	}
 	va_end (args);
+	return (1);
+}
+
+int main()
+{
+	ft_printf("cou%cou %s %s", 'c', "jjjj", "Lqurq");
 }
